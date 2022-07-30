38afd3a4b1b73436ef97197723d996ee48368836
neosr <fernando@neosr.com>
2022-07-30 00:01:14 +0000

    Aula 6 final

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
 from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.exc import IntegrityError
 
+#variaveis de ambiente
+import os
+
 app = Flask("hello")
+
+#URL LOCAL
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+#URL externo
+#app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL") or "sqlite:///app.db"
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "mali"
 
@@ -87,6 +95,22 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/create', methods=["GET", "POST"])
+@login_required
+
+def create():
+    if request.method == "POST":
+        title   = request.form['title']
+        body    = request.form['body']
+        try:
+            post = Post(title=title, body=body, author=current_user)
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('index'))
+        except IntegrityError:
+            flash("Error on create Post, try again later")
+    return render_template('create.html')
 
 
 
